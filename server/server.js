5c4329cb0113ce3a861a6f32ae7ad40b4e70a7ce
Satyam kumar <jhajhasatyam100@gmail.com>
2026-07-05 01:27:06 +0530

    origin setup

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(epxress.json({limit: "10mb"} ));
 
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",'https://baat-cheet-nine.vercel.app'],
     credentials:true
 }))
 app.use("/api/auth",authRoutes)
