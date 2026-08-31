05d38feca80e0ca24849bb0fc55384b57cff69e7
Satyam Kumar <alakjarasatyam@gmail.com>
2026-08-31 17:18:01 +0530

    minor fixes

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -12,3 +12,32 @@ export function formatMessageDateTime(date) {
     timeStyle: "short",
   });
 }
+
+export function formatMessageDate(date) {
+  const messageDate = new Date(date);
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  if (
+    messageDate.getDate() === today.getDate() &&
+    messageDate.getMonth() === today.getMonth() &&
+    messageDate.getFullYear() === today.getFullYear()
+  ) {
+    return "Today";
+  }
+
+  if (
+    messageDate.getDate() === yesterday.getDate() &&
+    messageDate.getMonth() === yesterday.getMonth() &&
+    messageDate.getFullYear() === yesterday.getFullYear()
+  ) {
+    return "Yesterday";
+  }
+
+  return messageDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
